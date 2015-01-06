ffb3832f600e77cc96fd648d2728c42416f7368d
ErikvanderBijl <erikvdbijl@gmail.com>
2015-01-06 21:56:19 +0100

    First lines of code

--- GUI.py
+++ GUI.py
@@ -1,9 +1,16 @@
+import Tkinter as tk
 
+class GUI():
+    def __init__(self):
+        print 'GUI::init'
+
+    def run(self):
+        print 'ǴUI::run'
 
 def main():
     print 'test'
-
-    print 'Github Commit test'
+    gui = GUI()
+    gui.run()
 
 if __name__=="__main__":
     main()
